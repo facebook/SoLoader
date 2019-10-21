cdd144ab84d7af8c370a4a0e1e6b7ce5d7e19d5c
Andres Suarez <asuarez@fb.com>
2019-10-21 12:21:47 -0700

    Yearless license header
    Differential Revision: D18041824
    
    fbshipit-source-id: 8c6a39294d2983b12d5f5b3fbbccd1c453f776b4

--- java/com/facebook/soloader/genstructs.sh
+++ java/com/facebook/soloader/genstructs.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 #
 # This script generates Java structures that contain the offsets of
@@ -26,13 +39,13 @@ EOF
     gcc -g -c -o elfhdr.o elfhdr.c
     cat > $struct.java <<EOF
 /*
- * Copyright 2014-present Facebook, Inc.
+ * Copyright (c) Facebook, Inc. and its affiliates.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
