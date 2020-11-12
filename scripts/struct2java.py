57502e290685c11916cdf86bb5674fd8c19841c8
Andres Suarez <asuarez@fb.com>
2020-11-12 08:46:41 -0800

    Apply BLACK
    Reviewed By: aniketmathur
    
    Differential Revision: D24900633
    
    fbshipit-source-id: 870e6045e29be29919b0eb8539d9f6f72743664f

--- scripts/struct2java.py
+++ scripts/struct2java.py
@@ -20,68 +20,73 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
-import sys
+
 import argparse
+import sys
+
 from elftools.elf.elffile import ELFFile
 
-parser = argparse.ArgumentParser(description='Output struct layout')
-parser.add_argument('elffile', help='ELF file containing debug information')
-parser.add_argument('struct', help='Name of struct (or typedef of struct)')
+parser = argparse.ArgumentParser(description="Output struct layout")
+parser.add_argument("elffile", help="ELF file containing debug information")
+parser.add_argument("struct", help="Name of struct (or typedef of struct)")
 args = parser.parse_args()
 
+
 def die(fmt, *args):
-  sys.stderr.write(('struct2java.py: ' + fmt + '\n') % args)
-  sys.exit(1)
+    sys.stderr.write(("struct2java.py: " + fmt + "\n") % args)
+    sys.exit(1)
+
 
 def DIE_to_name(DIE):
-  name_attr = DIE.attributes.get('DW_AT_name')
-  if name_attr is not None:
-    return name_attr.value.decode('ascii')
+    name_attr = DIE.attributes.get("DW_AT_name")
+    if name_attr is not None:
+        return name_attr.value.decode("ascii")
 
-  return None
+    return None
 
-with open(args.elffile, 'rb') as f:
-  elffile = ELFFile(f)
-  if not elffile.has_dwarf_info():
-    die('file does not contain debug information')
 
-  dwarfinfo = elffile.get_dwarf_info()
+with open(args.elffile, "rb") as f:
+    elffile = ELFFile(f)
+    if not elffile.has_dwarf_info():
+        die("file does not contain debug information")
 
-  structs_by_offset = {}
-  structs_by_name = {}
-  typedefs_by_name = {}
+    dwarfinfo = elffile.get_dwarf_info()
 
-  for CU in dwarfinfo.iter_CUs():
-    for DIE in CU.iter_DIEs():
-      if DIE.tag == 'DW_TAG_typedef':
-        name = DIE_to_name(DIE)
-        if name is not None:
-          typedefs_by_name[name] = DIE
-      
-      if DIE.tag == 'DW_TAG_structure_type':
-        structs_by_offset[DIE.offset] = DIE
-        name = DIE_to_name(DIE)
-        if name is not None:
-          structs_by_name[name] = DIE
+    structs_by_offset = {}
+    structs_by_name = {}
+    typedefs_by_name = {}
 
-  struct = structs_by_name.get(args.struct)
-  
-  if struct is None:
-    td = typedefs_by_name.get(args.struct)
-    if td is not None:
-      struct_offset = td.attributes['DW_AT_type'].value + td.cu.cu_offset
-      struct = structs_by_offset.get(struct_offset)
+    for CU in dwarfinfo.iter_CUs():
+        for DIE in CU.iter_DIEs():
+            if DIE.tag == "DW_TAG_typedef":
+                name = DIE_to_name(DIE)
+                if name is not None:
+                    typedefs_by_name[name] = DIE
 
-  if struct is None:
-    die('could not find struct %s', args.struct)
+            if DIE.tag == "DW_TAG_structure_type":
+                structs_by_offset[DIE.offset] = DIE
+                name = DIE_to_name(DIE)
+                if name is not None:
+                    structs_by_name[name] = DIE
 
-  print('final class %s {' % args.struct)
-  for child_DIE in struct.iter_children():
-    if child_DIE.tag == 'DW_TAG_member':
-      name = DIE_to_name(child_DIE)
-      offset = child_DIE.attributes['DW_AT_data_member_location'].value
-      print('  public static final int %s = 0x%x;' % (name, offset))
-    else:
-      die('unknown child of struct DIE: %r', child_DIE)
-      
-  print('}')
+    struct = structs_by_name.get(args.struct)
+
+    if struct is None:
+        td = typedefs_by_name.get(args.struct)
+        if td is not None:
+            struct_offset = td.attributes["DW_AT_type"].value + td.cu.cu_offset
+            struct = structs_by_offset.get(struct_offset)
+
+    if struct is None:
+        die("could not find struct %s", args.struct)
+
+    print("final class %s {" % args.struct)
+    for child_DIE in struct.iter_children():
+        if child_DIE.tag == "DW_TAG_member":
+            name = DIE_to_name(child_DIE)
+            offset = child_DIE.attributes["DW_AT_data_member_location"].value
+            print("  public static final int %s = 0x%x;" % (name, offset))
+        else:
+            die("unknown child of struct DIE: %r", child_DIE)
+
+    print("}")
